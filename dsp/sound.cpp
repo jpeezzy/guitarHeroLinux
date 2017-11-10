81e1eb8e8e7ac07c269764ac701b50b9c37e8619
justin <jpeezzy@hotmail.com>
2017-11-09 18:03:39 -0800

    added dsp and threads

--- dsp/sound.cpp
+++ dsp/sound.cpp
@@ -8,6 +8,9 @@
 #include <iostream>
 #include <unistd.h>
 #include <fstream>
+#include <thread>
+#include <pthread.h>
+#include <mutex>
 using namespace std;
 
 #define TYPE char
@@ -17,21 +20,61 @@ using namespace std;
 #define CHANNELS 1 //number of audio channels
 #define PI 3.14159
 #define NUM_FREQS 3 //total number of frequencies
-#define BUFFSIZE (int) (NUM_FREQS*LENGTH*RATE*SIZE*CHANNELS) //bytes sent to audio device
+//#define BUFFSIZE (int) (NUM_FREQS*LENGTH*RATE*SIZE*CHANNELS) //bytes sent to audio device
+#define BUFFSIZE 10000
 #define ARRAYSIZE (int) (NUM_FREQS*LENGTH*RATE*CHANNELS) //total number of samples
 #define SAMPLE_MAX (pow(2,SIZE*8 - 1) - 1) 
-
-void writeToSoundDevice(TYPE buf[], int deviceID) {
-	int status;
-	status = write(deviceID, buf, BUFFSIZE);
-	if (status != BUFFSIZE)
-		perror("Wrote wrong number of bytes\n");
+std::mutex keyLock;
+void writeToSoundDevice(TYPE buf[], int deviceID, int status) {
+	//	status = write(deviceID, buf, BUFFSIZE);
+	//	if (status != BUFFSIZE)
+	//		perror("Wrote wrong number of bytes\n");
 	status = ioctl(deviceID, SNDCTL_DSP_SYNC, 0);
 	if (status == -1)
 		perror("SNDCTL_DSP_SYNC failed\n");
 }
 
+void initializeThread(int deviceID)
+{
+	TYPE soundBuf[BUFFSIZE];
+	std::ofstream outfile;
+	outfile.open("values.txt", std::ios_base::app);
+
+	for (int t = 0; t < BUFFSIZE; ++t) 
+	{
+		soundBuf[t] = 2*sin(880*PI*t/RATE);
+		//std::cout << (int)soundBuf[t] << std::endl;
+		outfile << sin(880*PI*t/RATE) <<"\n";
+	}
+
+	int status;
+	status = write(deviceID, soundBuf, BUFFSIZE);
+	printf("thread print line \n");
+	while(true)
+	{
+//		frequency = 440;
+		for (int t = 0; t < BUFFSIZE; ++t) 
+		{
+			//soundBuf[t] = 2*sin(frequency*2*PI*t/RATE);
+			soundBuf[t] = 2*sin(880*PI*t/RATE) +  2*sin(1100*PI*t/RATE) + 2*sin(1320*PI*t/RATE);;;
+			//std::cout << (int)soundBuf[t] << std::endl;
+			outfile << sin(880*PI*t/RATE) <<"\n";
+		}
+
+		int status;
+		status = write(deviceID, soundBuf, BUFFSIZE);
+		keyLock.lock();
+		writeToSoundDevice(soundBuf, deviceID, status);
+		keyLock.unlock();
+	}
+}
+
 int main() {
+	//for (i = 0; i < NUM_FREQS; ++i) {
+	//		switch (i)
+	//
+	// conceptual idea:: 2 threads, one buffering while the other is playing. 
+	//
 	int deviceID, arg, status, f, t, a, i;
 	TYPE buf[ARRAYSIZE];
 	deviceID = open("/dev/dsp", O_WRONLY, 0);
@@ -51,39 +94,13 @@ int main() {
 	if (status == -1)
 		perror("Unable to set sampling rate\n");
 	a = SAMPLE_MAX;
-	for (i = 0; i < NUM_FREQS; ++i) {
-		switch (i) {
-			case 0:
-				f = 262;
-				break;
-			case 1:
-				f = 330;
-				break;
-			case 2:
-				f = 392;
-				break;
-		}
-		for (t = 0; t < ARRAYSIZE/NUM_FREQS; ++t) {
-			buf[t + ((ARRAYSIZE / NUM_FREQS) * i)] = floor(a * 20*sin(2*PI*f*t/RATE));
-		}
-	}
-
-	TYPE soundBuf[ARRAYSIZE/NUM_FREQS];
-//for (i = 0; i < NUM_FREQS; ++i) {
-//		switch (i)
-//
-//
-	std::ofstream outfile;
-	outfile.open("values.txt", std::ios_base::app);
-	
-	for (t = 0; t < ARRAYSIZE/NUM_FREQS; ++t) {
-		soundBuf[t] = floor(sin(880*PI*t));
-		//std::cout << (int)soundBuf[t] << std::endl;
-		outfile << (int)soundBuf[t] <<"\n";
-	}
-	writeToSoundDevice(soundBuf, deviceID);
-
-
-//	}
+	std::cout << "A is " << a << std::endl; 
+	std::thread t1 (initializeThread, deviceID);
+	std::thread t2(initializeThread, deviceID);
+	t1.join();
+	t2.join();
+	return 0;
 }
+
+
 
