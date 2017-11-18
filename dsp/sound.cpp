2ab4ce2099f6fc7d6ba89e6afd061cc20e086144
justin <jpeezzy@hotmail.com>
2017-11-18 00:14:01 -0800

    started fourier analysis

--- dsp/sound.cpp
+++ dsp/sound.cpp
@@ -16,6 +16,7 @@ using namespace std;
 #define TYPE char
 #define LENGTH 1 //number of seconds per frequency
 #define RATE 44100 //sampling rate
+#define SCALE 1000
 #define SIZE sizeof(TYPE) //size of sample, in bytes
 #define CHANNELS 1 //number of audio channels
 #define PI 3.14159
@@ -28,8 +29,8 @@ std::mutex keyLock;
 static int status;
 static TYPE soundBuf[BUFFSIZE]; 
 
-void writeToSoundDevice(int deviceID) {
-
+void writeToSoundDevice(int deviceID) 
+{
 	status = ioctl(deviceID, SNDCTL_DSP_SYNC, 0);
 	if (status == -1)
 		perror("SNDCTL_DSP_SYNC failed\n"); 
@@ -45,17 +46,67 @@ void initializeThread(int deviceID)
 	std::ofstream outfile;
 	outfile.open("values.txt", std::ios_base::app);
 	int status;
-	while(true)
-	{
+	//while(true)
+//	{
 		int frequency = 440;
-		for (int t = 0; t < BUFFSIZE; ++t) 
+		int amp;
+		//new goal, read graph for fourier analysis from hz = 0 -> 10000; then get each amplitude and place them inside loop Iwork on sound later);
+		for (int i = 0; i < 10; i++)
 		{
-			//soundBuf[t] = 2*sin(frequency*2*PI*t/RATE);
-			soundBuf[t] = 2*sin(880*PI*t/RATE) +  2*sin(1100*PI*t/RATE) + 2*sin(1320*PI*t/RATE);;;
-			//std::cout << (int)soundBuf[t] << std::endl;
-			//outfile << sin(880*PI*t/RATE) <<"\n";
+			switch(i)
+			{
+				case 0: 
+					amp = 0.05*SCALE;
+					frequency = 55;
+					break;
+				case 1:
+					amp = 0.005*SCALE;
+					frequency = 110;
+					break;
+				case 2:
+					amp = 0.1*SCALE;
+					frequency = 165;
+					break;
+				case 3:
+					amp = 0.01*SCALE;
+					frequency = 220;
+					break;
+				case 4:
+					amp = 0.05*SCALE;
+					frequency = 275;
+					break;
+				case 5:
+					amp = 0.05*SCALE;
+					frequency = 330;
+					break;
+				case 6:
+					amp = 0.01*SCALE;
+					frequency = 385;
+					break;
+				case 7:
+					amp = 0.005*SCALE;
+					frequency = 440;
+					break;
+				case 8:
+					amp = 0.005*SCALE;
+					frequency = 495;
+					break;
+				case 9: 
+					amp = 0.005*SCALE;
+					frequency = 550;
+					break;
+
+			}
+			for (int t = 0; t < BUFFSIZE; ++t) 
+			{
+				soundBuf[t] +=amp*sin(frequency*2*PI*t/RATE);
+				//soundBuf[t] = 200*sin(frequency*2*PI*t/RATE);
+				//soundBuf[t] = 2*sin(880*PI*t/RATE) +  2*sin(1100*PI*t/RATE) + 2*sin(1320*PI*t/RATE);;;
+				//std::cout << (int)soundBuf[t] << std::endl;
+				//outfile << sin(880*PI*t/RATE) <<"\n";
+			}
 		}
-	}
+	//}
 }
 
 int main() {
